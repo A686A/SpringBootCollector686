926d665432803cc7db165c38697d405abd35e8e6
sonei666888 <1144284812@qq.com>
2023-07-30 02:18:20 +0800

    4/7/30 finsh

--- src/main/java/com/example/springbootdemo/controller/HelloController.java
+++ src/main/java/com/example/springbootdemo/controller/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.springbootdemo.controller;
 
 
+import com.example.springbootdemo.entity.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-@GetMapping("/hello")
+    @GetMapping("/hello")
 //http://localhost:8080/hello
-    public String hello(){
-    return "hhah sunrui";
-}
+    public String hello() {
+
+        return "hhah sunrui";
+    }
+
+    @PostMapping("/helloUser")
+//http://localhost:8080/helloUser
+    public String helloUser(String userName, String passWord) {
+        System.out.println("hhah sunrui" + userName + passWord);
+        return "hhah sunrui" + userName + passWord;
+    }
+
+
+    @PostMapping("/helloUser2")
+//http://localhost:8080/helloUser2
+    public String helloUser2(User user) {
+        System.out.println(user);
+        System.out.println(user.getUserName());
+        return "hhah sunrui" + user;
+    }
 }

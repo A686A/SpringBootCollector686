8089b01193d422a9e446197213fa8002331a8f19
sonei666888 <1144284812@qq.com>
2023-08-21 22:14:23 +0800

    23.8.21 Content-type error

--- src/main/java/com/example/springbootdemo/controller/RestTemplateController.java
+++ src/main/java/com/example/springbootdemo/controller/RestTemplateController.java
@@ -3,12 +3,14 @@ package com.example.springbootdemo.controller;
 import com.example.springbootdemo.handler.CustomErrorHandler;
 import com.example.springbootdemo.entity.Group;
 
+import com.sun.istack.internal.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -61,4 +63,13 @@ public class RestTemplateController {
                 return "error with code : " + responseEntity.getStatusCodeValue();
             }
         }
+
+    //http://localhost:8080/RestTemplateDemo
+    @GetMapping("/RestTemplateDemo")
+    public String restTemplateDemo(@NotNull@RequestHeader(value = "Content-Type")String contentType){
+
+        System.out.println(contentType);
+        return contentType;
+    }
+
 }

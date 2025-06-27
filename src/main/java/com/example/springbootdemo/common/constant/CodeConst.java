64b7c3c93865bc8ebe7da4eb05eec7b1260d6b66
sonei <sonei666888@gmail.com>
2025-06-28 00:20:23 +0900

    250628_new test

--- src/main/java/com/example/springbootdemo/common/constant/CodeConst.java
+++ src/main/java/com/example/springbootdemo/common/constant/CodeConst.java
@@ -2,7 +2,7 @@ package com.example.springbootdemo.common.constant;
 
 public class CodeConst {
     public static final String USER_ROLE = "o";
-
+    public static final String USER_NAME = "a";
     private CodeConst() {
 
     }
